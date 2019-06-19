670c40ac034dd47c4fc40d1efc00131b391a8471
taleb <taleb.r75@gmail.com>
2019-06-19 15:45:29 +0430

    first commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# Morph Bottom Navigation  
+[![](https://jitpack.io/v/TalebRafiepour/BottomNavigation.svg)](https://jitpack.io/#TalebRafiepour/BottomNavigation)
+
+
+This library represents a Bottom Navigation with an awesome morph effect on top of the selected item.
+
+![ezgif com-crop](https://user-images.githubusercontent.com/15737675/41735760-d633e706-758a-11e8-9f30-3f07c8ed4371.gif)
+
+Don't forget to star the project if you like it! 
+![star](https://user-images.githubusercontent.com/15737675/39397370-85f5b294-4afe-11e8-9c02-0dfdf014136a.png)
+ == ![heart](https://user-images.githubusercontent.com/15737675/39397367-6e312c2e-4afe-11e8-9fbf-32001b0165a1.png)
+ 
+ And feel free to submit issues and enhancement requests !
+ 
+ ![ezgif com-video-to-gif](https://user-images.githubusercontent.com/15737675/41736506-026b6fd6-758d-11e8-9be6-7bc217aaa1e8.gif)
+![ezgif com-video-to-gif 1](https://user-images.githubusercontent.com/15737675/41736508-03b14cf8-758d-11e8-8bef-3909528f405d.gif)
+![ezgif com-video-to-gif 2](https://user-images.githubusercontent.com/15737675/41736510-04dc9ce0-758d-11e8-9951-11d924ebc6b0.gif)
+
+
+## How to
+#### Gradle
+```Gradle
+dependencies {
+    implementation 'com.github.TalebRafiepour:BottomNavigation:1.0.2'
+}
+
+
+allprojects {
+		repositories {
+			...
+			maven { url 'https://jitpack.io' }
+		}
+	}
+```
+#### Usage
+`MorphBottomNavigationView` extends the `BottomNavigationView` from the official [Google Material Component repository](https://github.com/material-components/material-components-android), so you can use it as described on the [Android Developers documentation](https://developer.android.com/reference/com/google/android/material/bottomnavigation/BottomNavigationView)
+
+#### In your XML layout
+```Xml
+<com.tbuonomo.morphbottomnavigation.MorphBottomNavigationView
+    android:id="@+id/bottomNavigationView"
+    android:layout_width="match_parent"
+    android:layout_height="wrap_content"
+    app:layout_constraintBottom_toBottomOf="parent"
+    app:menu="@menu/menu_bottom_navigation"
+    app:backgroundTint="@color/colorPrimary"
+    app:morphCornerRadius="128dp"
+    app:morphItemRadius="64dp"
+    app:morphVerticalOffset="8dp"
+    style="@style/BottomNavigationView"
+    />
+```
+
+#### Custom Attributes
+| Attribute | Description |
+| --- | --- |
+| `backgroundTint` | Color of the bottom navigation background |
+| `morphCornerRadius` | Radius in dp of the morph corners (by default 128dp) |
+| `morphItemRadius` | Radius in dp of the morph item circle shape (by default 64dp) |
+| `morphVerticalOffset` | The vertical offset of the morph shape above the bottom navigation (by default 8dp) |
+
+
+
+## License
+    Copyright 2018 Tommy Buonomo
+    
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
---
+++ morph-bottom-navigation/src/main/java/com/tbuonomo/morphbottomnavigation/MorphBottomNavigationView.kt
@@ -0,0 +1,246 @@
+package com.tbuonomo.morphbottomnavigation
+
+import android.animation.ValueAnimator
+import android.content.Context
+import android.graphics.*
+import android.graphics.Paint.Style
+import android.graphics.Paint.Style.STROKE
+import android.util.AttributeSet
+import android.util.Log
+import android.util.TypedValue
+import android.view.animation.DecelerateInterpolator
+import android.widget.FrameLayout
+import androidx.core.view.children
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.shape.ShapePathModel
+import java.lang.Exception
+import android.graphics.Typeface
+import android.view.*
+import com.google.android.material.internal.BaselineLayout
+import android.widget.TextView
+import androidx.core.view.get
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import java.lang.reflect.Field
+
+
+class MorphBottomNavigationView : BottomNavigationView, OnNavigationItemSelectedListener {
+  private val paint = Paint().apply {
+    isAntiAlias = true
+    color = Color.BLUE
+    strokeWidth = 2f
+    style = STROKE
+    alpha = 50
+  }
+  private val height: Float
+
+  private val topEdgeTreatment: MorphBottomNavigationViewTopEdgeTreatment
+  private val materialShapeDrawable: MaterialShapeDrawable
+
+  private var selectedItem = 0
+
+  private var selectionAnimator: ValueAnimator? = null
+
+  private var fontSrc:String? = null
+
+  var morphItemRadius: Float = 0f
+    set(radius) {
+      field = radius
+      topEdgeTreatment.morphItemRadius = radius
+      invalidate()
+    }
+
+  var morphCornerRadius: Float = 0f
+    set(radius) {
+      field = radius
+      topEdgeTreatment.morphCornerRadius = radius
+      invalidate()
+    }
+
+  var morphVerticalOffset: Float = 0f
+    set(offset) {
+      field = offset
+      topEdgeTreatment.morphVerticalOffset = offset
+      if (layoutParams != null) {
+        layoutParams.height = (height + morphVerticalOffset).toInt()
+      }
+
+      invalidate()
+    }
+
+  var drawDebug: Boolean = false
+    set(enable) {
+      field = enable
+      invalidate()
+    }
+
+  constructor(context: Context) : this(context, null)
+  constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+
+  private var bottomNavigationMenuView: BottomNavigationMenuView
+
+  constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs,
+          defStyleAttr) {
+
+    val a = resources.obtainAttributes(attrs, R.styleable.MorphBottomNavigationView)
+
+    val typedValue = TypedValue()
+    context.theme?.resolveAttribute(R.attr.colorPrimary, typedValue, true)
+    val backgroundTint = a.getColor(R.styleable.MorphBottomNavigationView_backgroundTint, typedValue.data)
+
+    val morphItemRadius = a.getDimensionPixelSize(R.styleable.MorphBottomNavigationView_morphItemRadius, dpToPx(64f).toInt()).toFloat()
+    val morphVerticalOffset = a.getDimensionPixelSize(R.styleable.MorphBottomNavigationView_morphVerticalOffset, dpToPx(8f).toInt()).toFloat()
+    val morphCornerRadius = a.getDimensionPixelSize(R.styleable.MorphBottomNavigationView_morphCornerRadius, dpToPx(128f).toInt()).toFloat()
+    fontSrc = a.getString(R.styleable.MorphBottomNavigationView_fontAssetSrc)
+    a.recycle()
+
+    height = dpToPx(56f)
+
+
+    bottomNavigationMenuView = getChildAt(0) as BottomNavigationMenuView
+
+    topEdgeTreatment = MorphBottomNavigationViewTopEdgeTreatment(
+            bottomNavigationMenuView,
+            morphItemRadius,
+            morphVerticalOffset,
+            morphCornerRadius)
+
+    this.morphItemRadius = morphItemRadius
+    this.morphVerticalOffset = morphVerticalOffset
+    this.morphCornerRadius = morphCornerRadius
+
+    val shapePathModel = ShapePathModel()
+    shapePathModel.topEdge = topEdgeTreatment
+    materialShapeDrawable = MaterialShapeDrawable(shapePathModel)
+    materialShapeDrawable.isShadowEnabled = true
+    materialShapeDrawable.paintStyle = Style.FILL
+    materialShapeDrawable.setTint(backgroundTint)
+
+    background = materialShapeDrawable
+    val menuParams = bottomNavigationMenuView.layoutParams as FrameLayout.LayoutParams
+    menuParams.gravity = (Gravity.BOTTOM or Gravity.CENTER_HORIZONTAL)
+
+    setOnNavigationItemSelectedListener(this)
+
+    setWillNotDraw(false)
+  }
+
+  override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+    super.onLayout(changed, left, top, right, bottom)
+    setFont(fontSrc)
+  }
+
+
+  fun setFont(fontSrc: String?){
+    this.fontSrc = fontSrc
+    if (fontSrc == null)
+      return
+    try {
+      val typeface = Typeface.createFromAsset(context!!.assets,fontSrc)
+      setTypeFace(typeface)
+    }catch (e: Exception){
+      e.printStackTrace()
+    }
+  }
+
+  private fun setTypeFace(typeface: Typeface){
+    val bottomMenu = getChildAt(0) as ViewGroup
+    val bottomMenuChildCount = bottomMenu.childCount
+    var item: BottomNavigationItemView
+    var itemTitle: View
+    val shiftingMode: Field
+
+
+    try {
+      //if you want to disable shiftingMode:
+      //shiftingMode is a private member variable so you have to get access to it like this:
+      shiftingMode = bottomMenu.javaClass.getDeclaredField("mShiftingMode")
+      shiftingMode.isAccessible = true
+      shiftingMode.setBoolean(bottomMenu, false)
+      shiftingMode.isAccessible = false
+    } catch (e: NoSuchFieldException){
+      e.printStackTrace()
+    } catch (e: IllegalAccessException){e.printStackTrace();}
+
+    for(i in 0 until bottomMenuChildCount){
+      item = bottomMenu.getChildAt(i) as BottomNavigationItemView
+      //this shows all titles of items
+      //every BottomNavigationItemView has two children, first is an itemIcon and second is an itemTitle
+      itemTitle = item.getChildAt(1);
+      //every itemTitle has two children, first is a smallLabel and second is a largeLabel. these two are type of AppCompatTextView
+      ((itemTitle as BaselineLayout).getChildAt(0) as TextView).setTypeface(typeface, Typeface.BOLD)
+      ((itemTitle as BaselineLayout).getChildAt(1) as TextView).setTypeface(typeface, Typeface.BOLD)
+    }
+  }
+
+
+
+
+
+  /**
+   * Proxy for listener
+   */
+  override fun setOnNavigationItemSelectedListener(listener: OnNavigationItemSelectedListener?) {
+    super.setOnNavigationItemSelectedListener {
+      onNavigationItemSelected(it)
+      if (listener !is MorphBottomNavigationView) {
+        listener?.onNavigationItemSelected(it)
+      }
+      true
+    }
+  }
+
+  override fun onNavigationItemSelected(item: MenuItem): Boolean {
+    val indexOfItemSelected = menu.indexOfItem(item)
+    if (indexOfItemSelected != selectedItem) {
+      topEdgeTreatment.lastSelectedItem = selectedItem
+      topEdgeTreatment.selectedItem = indexOfItemSelected
+      selectedItem = indexOfItemSelected
+
+      selectionAnimator?.end()
+      selectionAnimator?.cancel()
+
+      selectionAnimator = ValueAnimator.ofFloat(0f, 1f)
+      selectionAnimator?.addUpdateListener {
+        materialShapeDrawable.interpolation = it.animatedValue as Float
+      }
+
+      selectionAnimator?.duration = 200
+      selectionAnimator?.interpolator = DecelerateInterpolator()
+
+      selectionAnimator?.start()
+    }
+
+    return true
+  }
+
+  override fun onAttachedToWindow() {
+    super.onAttachedToWindow()
+    // Change height
+    layoutParams.height = (height + morphVerticalOffset).toInt()
+  }
+
+  override fun onDraw(canvas: Canvas) {
+    super.onDraw(canvas)
+    if (drawDebug) {
+      topEdgeTreatment.drawDebug(canvas, paint)
+    }
+  }
+
+  private fun dpToPx(dp: Float): Float {
+    return resources.displayMetrics.density * dp
+  }
+
+  private fun Menu.indexOfItem(item: MenuItem): Int {
+    for (i in 0 until this.size()) {
+      val menuItem = menu.getItem(i)
+      if (menuItem.itemId == item.itemId) {
+        return i
+      }
+    }
+    return -1
+  }
+}
+
